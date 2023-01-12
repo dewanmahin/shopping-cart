a7612bd6f14350bb7ea59eb56f7aef3c381feeaa
DEWAN <dewanmahinchy@gmail.com>
2023-01-12 20:07:41 +0600

    phone cart plus, minus & total price code added

--- js/phone.js
+++ js/phone.js
@@ -1,15 +1,29 @@
-document.getElementById('phone-plus').addEventListener('click', function(){
+function updatePhoneQuantity(isIncrease){
     const phoneQuantityField = document.getElementById('phone-number');
     const phoneQuantityString = phoneQuantityField.value;
     let previousPhoneQuantity = parseInt(phoneQuantityString);
-    const newPhoneQuantity = ++previousPhoneQuantity;
+    let newPhoneQuantity;
+    if(isIncrease == true){
+        newPhoneQuantity = ++previousPhoneQuantity;
+    }else{
+        newPhoneQuantity = --previousPhoneQuantity;
+    }
     phoneQuantityField.value = newPhoneQuantity;
+    return newPhoneQuantity;
+}
+
+function updatePhonePrice(newPhoneQuantity){
+    const phoneTotalPrice = newPhoneQuantity * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document.getElementById('phone-plus').addEventListener('click', function(){
+    const newPhoneQuantity = updatePhoneQuantity(true);
+    updatePhonePrice(newPhoneQuantity)
 })
 
 document.getElementById('phone-minus').addEventListener('click', function(){
-    const phoneQuantityField = document.getElementById('phone-number');
-    const phoneQuantityString = phoneQuantityField.value;
-    let previousPhoneQuantity = parseInt(phoneQuantityString);
-    const newPhoneQuantity = --previousPhoneQuantity;
-    phoneQuantityField.value = newPhoneQuantity;
+    const newPhoneQuantity = updatePhoneQuantity(false);
+    updatePhonePrice(newPhoneQuantity)
 })
